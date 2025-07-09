c811e3c4465301d834d6c173f1e1fae2db4abf79
EmmanuelOZ <davidozuna84@hotmail.com>
2025-07-09 11:33:04 -0600

    lista de restaurantes

---
+++ src/models/restaurant.ts
@@ -0,0 +1,9 @@
+interface Restaurant {
+  locationId: number;
+  heroImgUrl: string;
+  name: string;
+  averageRating: number;
+  userReviewCount: number;
+  currentOpenStatusCategory: string;
+  currentOpenStatusText: string;
+}
